397f4b2b457049c31c4b1c85f930f59201986926
Gregoire HERBIN <melozy@free.fr>
2021-02-05 12:00:12 +0100

    Gestion des annonces

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,27 +3,57 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\addProvider;
+use WW\Faker\Provider\Picture;
+
 
 class AppFixtures extends Fixture
 {
 	public function load(ObjectManager $manager)
 	{ 
+		$faker = Factory::create();
+		$faker -> addProvider ( new Picture ( $faker ));
+
+		
+
 		for($i=1;$i<=30;$i++){
 			$ad = new Ad();
 
-			$ad ->setTitle("Titre de l'annonce n°$i")
-			->setSlug("titre-de-l-annonce-n-$i")
-			->setCoverImage("http://placehold.it/1000x300")
-			->setIntroduction("Bonjour à tous c'est une introduction")
-			->setContent("<p>Je suis un contenu riche !</p>")
+			$title = $faker -> sentence();
+			$coverImage = $faker -> pictureUrl(1000,350)."?".mt_rand(1,100);
+			$introduction = $faker -> paragraph(2);
+			$content = '<p>' . join ('</p><p>', $faker->paragraphs(5)) . '</p>';
+
+
+			$ad ->setTitle($title)
+			->setCoverImage($coverImage)
+			->setIntroduction($introduction)
+			->setContent($content)
 			->setPrice(mt_rand(40,200))
-				->setRooms(mt_rand(1,5));
+			->setRooms(mt_rand(1,5));
 
-				$manager->persist($ad);
-			}	
+		for ($j=1;$j<=mt_rand(2,5);$j++){
 
-			$manager->flush();
+			$image = new Image();
+
+			$image ->setUrl($faker->pictureUrl(1000,350)."?".mt_rand(1,100))
+				   ->setCaption($faker->sentence())	
+				   ->setAd($ad);
+
+	    $manager->persist($image);
+
+
+		}	
+
+
+
+			$manager->persist($ad);
+		}	
+
+		$manager->flush();
 	}
 }
